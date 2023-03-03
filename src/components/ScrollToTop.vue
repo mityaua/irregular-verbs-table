22a4e9cffe76e21ed7087cb0c699b570bc8d0208
Dmytro Shukaliuk <mityaua@gmail.com>
2023-03-03 09:16:17 +0200

    Cleared images and updated to top icon

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -1,17 +1,6 @@
 <template>
-  <div id="pagetop" class="fixed right-0 bottom-0 cursor-pointer" title="Scroll to top" v-show="scrollY > 300" @click="toTop">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="#4a5568"
-      stroke-width="1"
-      stroke-linecap="square"
-    >
-      <path d="M18 15l-6-6-6 6" />
-    </svg>
+  <div id="pagetop" class="fixed right-2 bottom-2 cursor-pointer" title="Scroll to top" v-show="scrollY > 300" @click="toTop">
+    <img src="../assets/up-arrow.svg" alt="Scroll to top" width="48" height="48" />
   </div>
 </template>
 
