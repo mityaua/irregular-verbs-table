97961af8c85773cb31e4da4e934c102cc5a34e9d
Dmytro Shukaliuk <mityaua@gmail.com>
2024-01-24 18:17:06 +0200

    Icons impovements

--- src/components/VerbsTable.vue
+++ src/components/VerbsTable.vue
@@ -5,6 +5,7 @@ import { verbs } from "../data/verbs.json";
 import TableRow from "./TableRow.vue";
 import SearchInput from "../components/SearchInput.vue";
 import SearchResults from "../components/SearchResults.vue";
+import SortIcon from "../assets/sort-icon.svg";
 
 const defaultSortColumn = "infinitive";
 
@@ -55,18 +56,9 @@ const onSort = (columnName: string): void => {
           <th width="33%" scope="col" class="p-3 bg-gray-50 dark:bg-gray-700">
             <div class="flex justify-center items-center">
               <p>Infinitive</p>
-              <a href="#" aria-label="Sort by infinitive" class="ease-in duration-300" @click="onSort('infinitive')"
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-3 h-3 ml-1"
-                  aria-hidden="true"
-                  :fill="sortByColumn === 'infinitive' ? sortIconColor : 'currentColor'"
-                  viewBox="0 0 320 512"
-                >
-                  <path
-                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"
-                  /></svg
-              ></a>
+              <a href="#" aria-label="Sort by infinitive" class="ease-in duration-300" @click="onSort('infinitive')">
+                <SortIcon class="w-3 h-3 ml-1" :fill="sortByColumn === 'infinitive' ? sortIconColor : 'currentColor'" />
+              </a>
             </div>
           </th>
 
@@ -74,18 +66,9 @@ const onSort = (columnName: string): void => {
           <th width="33%" scope="col" class="p-3 bg-gray-50 dark:bg-gray-700">
             <div class="flex justify-center items-center">
               <p>Past simple</p>
-              <a href="#" aria-label="Sort by past simple" class="ease-in duration-300" @click="onSort('pastSimple')"
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-3 h-3 ml-1"
-                  aria-hidden="true"
-                  :fill="sortByColumn === 'pastSimple' ? sortIconColor : 'currentColor'"
-                  viewBox="0 0 320 512"
-                >
-                  <path
-                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"
-                  /></svg
-              ></a>
+              <a href="#" aria-label="Sort by past simple" class="ease-in duration-300" @click="onSort('pastSimple')">
+                <SortIcon class="w-3 h-3 ml-1" :fill="sortByColumn === 'pastSimple' ? sortIconColor : 'currentColor'" />
+              </a>
             </div>
           </th>
 
@@ -93,18 +76,9 @@ const onSort = (columnName: string): void => {
           <th width="33%" scope="col" class="p-3 bg-gray-50 dark:bg-gray-700">
             <div class="flex justify-center items-center">
               <p>Past participle</p>
-              <a href="#" aria-label="Sort by past participle" class="ease-in duration-300" @click="onSort('pastParticiple')"
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-3 h-3 ml-1"
-                  aria-hidden="true"
-                  :fill="sortByColumn === 'pastParticiple' ? sortIconColor : 'currentColor'"
-                  viewBox="0 0 320 512"
-                >
-                  <path
-                    d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"
-                  /></svg
-              ></a>
+              <a href="#" aria-label="Sort by past participle" class="ease-in duration-300" @click="onSort('pastParticiple')">
+                <SortIcon class="w-3 h-3 ml-1" :fill="sortByColumn === 'pastParticiple' ? sortIconColor : 'currentColor'" />
+              </a>
             </div>
           </th>
         </tr>
