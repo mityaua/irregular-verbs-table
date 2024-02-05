4b486c381eb9b6a85739b6ce6bc7f081821cde23
Dmytro Shukaliuk <mityaua@gmail.com>
2024-02-05 20:47:59 +0200

    Added visibleColumnName computed value

--- src/components/TableHead.vue
+++ src/components/TableHead.vue
@@ -1,9 +1,9 @@
 <template>
   <th width="33%" scope="col" class="p-3 bg-gray-50 dark:bg-gray-700">
     <div class="flex justify-center items-center">
-      <p>{{ columnName }}</p>
+      <p>{{ visibleColumnName }}</p>
 
-      <a href="#" :aria-label="`Sort by ${columnName}`" class="ease-in duration-300" @click="onColumnClick">
+      <a href="#" :aria-label="`Sort by ${visibleColumnName}`" class="ease-in duration-300" @click="onColumnClick">
         <SortIcon class="w-3 h-3 ml-1" :fill="activeColumnName === columnName ? sortIconColor : 'currentColor'" />
       </a>
     </div>
@@ -12,7 +12,9 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import Columns from "../enums/Columns";
 import SortIcon from "../assets/sort-icon.svg";
+
 const props = defineProps<{ isDescending: boolean; activeColumnName: string; columnName: string }>();
 
 const emit = defineEmits<{
@@ -20,6 +22,23 @@ const emit = defineEmits<{
 }>();
 
 const sortIconColor = computed<string>(() => (props.isDescending ? "#8a2be2" : "#00CC99"));
+const visibleColumnName = computed<string>(() => {
+  let name = "";
+
+  switch (props.columnName) {
+    case Columns.Infinitive:
+      name = "Infinitive";
+      break;
+    case Columns.PastSimple:
+      name = "Past Simple";
+      break;
+    case Columns.PastParticiple:
+      name = "Past Participle";
+      break;
+  }
+
+  return name;
+});
 
 const onColumnClick = () => {
   emit("click:column", props.columnName);
