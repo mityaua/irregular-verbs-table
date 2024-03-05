90b6cd92df58b6c86ffeca38a7ad616f3d550670
Dmytro Shukaliuk <mityaua@gmail.com>
2024-03-05 12:19:27 +0200

    Updated packages and added test google tag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
+import VueGtag from "vue-gtag";
 import "./index.css";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(VueGtag, {
+  config: { id: "G-0JXW9ZJB5B" },
+});
+app.mount("#app");
