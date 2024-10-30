dc9a4e6086f99ccfe3959429c04c13dc7e77ed1d
Dmytro Shukaliuk <mityaua@gmail.com>
2024-10-30 09:04:20 +0200

    Added additional dev libs

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/irregular-verbs-table/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-    },
-  },
-  plugins: [vue(), svgLoader()],
+	base: "/irregular-verbs-table/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@components": path.resolve(__dirname, "./src/components"),
+		},
+	},
+	plugins: [vue(), mkcert(), svgLoader()],
 });
