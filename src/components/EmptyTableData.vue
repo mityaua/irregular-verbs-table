7eb4b52955bfa14f898e1ce4947b0e2ae9db10c3
Dmytro Shukaliuk <mityaua@users.noreply.github.com>
2025-01-30 17:14:31 +0200

    Styles order and additional animation

--- src/components/EmptyTableData.vue
+++ src/components/EmptyTableData.vue
@@ -5,6 +5,6 @@ import EmptySearch from "@assets/empty-search.svg";
 <template>
 	<div class="flex items-center justify-center py-4 text-lg">
 		<span>Nothing found</span>
-		<EmptySearch class="ml-2" width="35" height="35" />
+		<EmptySearch class="ml-2 animate-pulse" width="35" height="35" />
 	</div>
 </template>
