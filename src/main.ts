189a749d5b6909797edb64ba6c28614feb613c40
Dmytro Shukaliuk <mityaua@users.noreply.github.com>
2025-05-30 17:43:28 +0300

    Update gtag config and some libs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from "vue";
-import { createGtag } from "vue-gtag";
+import { configure } from "vue-gtag";
 import "./assets/css/index.css";
 import "./style.css";
 import App from "./App.vue";
 
-const gtag = createGtag({
+configure({
+	appName: "EVT",
 	tagId: import.meta.env.VITE_GA_ID,
 });
 
 const app = createApp(App);
-app.use(gtag);
 app.mount("#app");
